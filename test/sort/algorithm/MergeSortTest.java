b13797e022cfd3f79c27975bc79cb14fdf4637e5
Tomislav Marinkovic <tmarin@thoughtworks.com>
2018-05-19 01:24:40 +0200

    Remove calculation of runtime on tests

--- test/sort/algorithm/MergeSortTest.java
+++ test/sort/algorithm/MergeSortTest.java
@@ -10,7 +10,7 @@ class MergeSortTest {
     @Test
     void shouldSort() {
         int[] data = new int[]{1,2,5,7,12,6,7,8,9};
-        int[] actual = new MergeSort(true).run(new Input(data, 4));
+        int[] actual = new MergeSort(false).run(new Input(data, 4));
         int[] expected = new int[]{1,2,5,6,7,7,8,9,12};
 
         assertArrayEquals(actual, expected);
