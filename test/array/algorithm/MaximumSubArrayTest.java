fb39e172746cc80a91b71c1d3d1298993559c2aa
Tomislav Marinkovic <tmarin@thoughtworks.com>
2018-05-27 16:31:40 +0200

    Add more test to maximum subarray problem

--- test/array/algorithm/MaximumSubArrayTest.java
+++ test/array/algorithm/MaximumSubArrayTest.java
@@ -9,10 +9,19 @@ import static org.junit.jupiter.api.Assertions.assertArrayEquals;
 class MaximumSubArrayTest {
 
     @Test
-    void shouldSort() {
+    void shouldFindMaximumSubArrayOfRandomArray() {
         int[] data = new int[]{13, -3, -25, 20, -3, -16, -23, 18, 20, -7, 12, -5, -22, 15, -4, 7};
         int[] actual = new MaximumSubArray(false).run(new DataInput(data));
         int[] expected = new int[]{18, 20, -7, 12};
+
+        assertArrayEquals(actual, expected);
+    }
+
+    @Test
+    void shouldFindMaximumSubArrayOfAllPositiveNumbers() {
+        int[] data = new int[]{1,2,3,4,5};
+        int[] actual = new MaximumSubArray(false).run(new DataInput(data));
+        int[] expected = new int[]{1,2,3,4,5};
 
         assertArrayEquals(actual, expected);
     }
